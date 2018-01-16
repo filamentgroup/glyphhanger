036577daa7496784932aa79920bce336954239a8
kennethormandy <hello@kennethormandy.com>
2018-01-16 11:52:02 -0800

    Improves initial CLI test and cleanup

--- test/cli.js
+++ test/cli.js
@@ -1,13 +1,26 @@
-const assert = require( "assert" );
-const path = require( "path" );
-const childProcess = require( "child_process" );
+var assert = require( "assert" );
+var path = require( "path" );
+var childProcess = require( "child_process" );
+var fs = require( "fs" );
+
+var fontPath = "test/fonts/sourcesanspro-regular.ttf"
+
+var removeFile = function( filePath ) {
+	fs.unlinkSync( filePath );
+}
 
 describe( "glyphhanger cli", function() {
-
-	it( "Produced a file", function (done) {
-		childProcess.exec('node index.js --whitelist=ABC --subset=test/fonts/sourcesanspro-regular.ttf', function(err) {
-      if (err) done(err)
-      else done();
+	it( "Produced a file", function ( done ) {
+		childProcess.exec(`node index.js --whitelist=ABC --subset=${fontPath} --formats=ttf`, function(err) {
+      if ( err ) {
+				done( err );
+			} else {
+				var subsetPath = fontPath.split( ".ttf" ).join( "-subset.ttf" )
+				var subset = fs.existsSync( subsetPath );
+				assert.ok( subset );
+				removeFile( subsetPath );
+				done();
+			};
 		})
 	})
 });
