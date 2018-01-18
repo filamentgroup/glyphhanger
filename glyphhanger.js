253f5d075bf41f8c46727d4fd91d235c1690e4e9
Zach Leatherman <zachleatherman@gmail.com>
2018-01-18 13:22:38 -0600

    Fixes #25.

--- glyphhanger.js
+++ glyphhanger.js
@@ -10,21 +10,63 @@
 
 	var GH = function() {
 		this.set = new CharacterSet();
+
+		if( typeof window !== "undefined" ) {
+			this.win = window;
+		}
+	};
+
+	GH.prototype.setEnv = function(win) {
+		this.win = win;
 	};
 
 	GH.prototype.init = function( parentNode ) {
 		if( parentNode ) {
-			this.findTextNodes( parentNode ).forEach( function( node ) {
-				this.saveGlyphs( node.nodeValue );
-			}.bind( this ) );
+			this.findTextNodes( parentNode ).filter(function( node ) {
+				// only non-empty values
+				return this.hasValue( node );
+			}.bind( this )).forEach( function( node ) {
+				this.saveGlyphs( this.getNodeValue( node ) );
+			}.bind( this ));
 		}
 	}
+
+	GH.prototype.fakeInnerText = function( node ) {
+		var value = node.nodeValue;
+
+		if( node.nodeType !== 3 ) {
+			return "";
+		}
+		if( node.parentNode ) {
+			var textTransform = this.win.getComputedStyle( node.parentNode ).getPropertyValue( "text-transform" );
+			switch (textTransform) {
+				case "uppercase":
+					return value.toUpperCase();
+				case "lowercase":
+					return value.toLowerCase();
+				case "capitalize":
+					// workaround language specific rules with text-transform
+					// "ß".toUpperCase() => "SS" in german, for example
+					return value.toUpperCase() + value.toLowerCase();
+			}
+		}
+
+		return value;
+	};
+
+	GH.prototype.getNodeValue = function( node ) {
+		return node.innerText || this.fakeInnerText( node );
+	};
+
+	GH.prototype.hasValue = function( node ) {
+		return (node.innerText || node.nodeValue).trim().length > 0;
+	};
 
 	GH.prototype.findTextNodes = function( node ) {
 		// via http://stackoverflow.com/questions/10730309/find-all-text-nodes-in-html-page
 		var all = [];
 		for( node = node.firstChild; node; node = node.nextSibling ) {
-			if( node.nodeType == 3 && !!node.nodeValue.trim() ) {
+			if( node.nodeType == 3 ) {
 				all.push( node );
 			} else {
 				all = all.concat( this.findTextNodes( node ) );
--- test/test.js
+++ test/test.js
@@ -17,10 +17,11 @@ describe( "glyphhanger", function() {
 		div.innerHTML = "abc";
 
 		var gh = new GlyphHanger();
+		gh.setEnv(window);
 		gh.init( div );
 
 		it( "should match", function() {
-			assert.equal( "abc", gh.toString() );
+			assert.equal( gh.toString(), "abc" );
 		});
 	});
 
@@ -29,22 +30,78 @@ describe( "glyphhanger", function() {
 		div.innerHTML = "<div>bcd</div>";
 
 		var gh = new GlyphHanger();
+		gh.setEnv(window);
 		gh.init( div );
 
 		it( "should match", function() {
-			assert.equal( "bcd", gh.toString() );
+			assert.equal( gh.toString(), "bcd" );
 		});
 	});
+
+	describe( "Node with text-transform: uppercase", function() {
+		var div = document.createElement( "div" );
+		div.innerHTML = "<div style='text-transform: uppercase'>bcd</div>";
+
+		var gh = new GlyphHanger();
+		gh.setEnv(window);
+		gh.init( div );
+
+		it( "should match", function() {
+			assert.equal( gh.toString(), "BCD" );
+		});
+	});
+
+	describe( "Complex node with text-transform: uppercase", function() {
+		var div = document.createElement( "div" );
+		div.innerHTML = "<div><span style='text-transform: uppercase'>b</span>cd</div>";
+
+		var gh = new GlyphHanger();
+		gh.setEnv(window);
+		gh.init( div );
+
+		it( "should match", function() {
+			assert.equal( gh.toString(), "Bcd" );
+		});
+	});
+
+	describe( "Node with text-transform: lowercase", function() {
+		var div = document.createElement( "div" );
+		div.innerHTML = "<div style='text-transform: lowercase'>BCD</div>";
+
+		var gh = new GlyphHanger();
+		gh.setEnv(window);
+		gh.init( div );
+
+		it( "should match", function() {
+			assert.equal( gh.toString(), "bcd" );
+		});
+	});
+
+	describe( "Node with text-transform: capitalize", function() {
+		var div = document.createElement( "div" );
+		div.innerHTML = "<div style='text-transform: capitalize'>test</div>";
+
+		var gh = new GlyphHanger();
+		gh.setEnv(window);
+		gh.init( div );
+
+		it( "should match", function() {
+			// TESTtest => unique and sorted becomes ESTest
+			assert.equal( gh.toString(), "ESTest" );
+		});
+	});
+
 
 	describe( "More complex node", function() {
 		var div = document.createElement( "div" );
 		div.innerHTML = "<div>bcd</div><div><span>e</span>fg</div>0123456789";
 
 		var gh = new GlyphHanger();
+		gh.setEnv(window);
 		gh.init( div );
 
 		it( "should match", function() {
-			assert.equal( "0123456789bcdefg", gh.toString() );
+			assert.equal( gh.toString(), "0123456789bcdefg" );
 		});
 	});
 
@@ -53,11 +110,12 @@ describe( "glyphhanger", function() {
 		div.innerHTML = "bcd";
 
 		var gh = new GlyphHanger();
+		gh.setEnv(window);
 		gh.saveGlyphs( "efgh")
 		gh.init( div );
 
 		it( "should match", function() {
-			assert.equal( "bcdefgh", gh.toString() );
+			assert.equal( gh.toString(), "bcdefgh" );
 		});
 	});
 
@@ -66,10 +124,11 @@ describe( "glyphhanger", function() {
 		div.innerHTML = "😎";
 
 		var gh = new GlyphHanger();
+		gh.setEnv(window);
 		gh.init( div );
 
 		it( "has a surrogate pair", function() {
-			assert.equal( "\\uD83D\\uDE0E", gh.toString() );
+			assert.equal( gh.toString(), "\\uD83D\\uDE0E" );
 		});
 	});
 
@@ -78,21 +137,63 @@ describe( "glyphhanger", function() {
 		div.innerHTML = "😎💩";
 
 		var gh = new GlyphHanger();
+		gh.setEnv(window);
 		gh.init( div );
 
 		it( "has two surrogate pairs", function() {
-			assert.equal( "\\uD83D\\uDCA9\\uD83D\\uDE0E", gh.toString() );
+			assert.equal( gh.toString(), "\\uD83D\\uDCA9\\uD83D\\uDE0E" );
 		});
 	});
 
-	describe( "Integration test: onload and DOMContentLoaded content", function() {
+	describe( "Integration test", function() {
 		var args = [ path.join( __dirname, "..", "phantomjs-glyphhanger.js" ), false, false, "", path.join( __dirname, "test.html" ) ];
+
+		it( "should have 3 distinct glyphs", function( done ) {
+			this.timeout( 30000 );
+			childProcess.execFile( phantomjs.path, args, function( error, stdout, stderr ) {
+
+				assert.equal( stdout.trim(), "abc" );
+				done();
+			});
+		})
+	});
+
+	describe( "Integration test: text-transform uppercase", function() {
+		var args = [ path.join( __dirname, "..", "phantomjs-glyphhanger.js" ), false, false, "", path.join( __dirname, "uppercase.html" ) ];
+
+		it( "should have uppercase words", function( done ) {
+			this.timeout( 30000 );
+			childProcess.execFile( phantomjs.path, args, function( error, stdout, stderr ) {
+
+				// abc ^word => abcWORD => DORWabc
+				assert.equal( stdout.trim(), "DORWabc" );
+				done();
+			});
+		})
+	});
+
+	describe( "Integration test: text-transform capitalize", function() {
+		var args = [ path.join( __dirname, "..", "phantomjs-glyphhanger.js" ), false, false, "", path.join( __dirname, "capitalize.html" ) ];
+
+		it( "should have capitalized words", function( done ) {
+			this.timeout( 30000 );
+			childProcess.execFile( phantomjs.path, args, function( error, stdout, stderr ) {
+
+				// abc ^word => abcWORDword => DORWabcdorw
+				assert.equal( stdout.trim(), "DORWabcdorw" );
+				done();
+			});
+		})
+	});
+
+	describe( "Integration test: onload and DOMContentLoaded content", function() {
+		var args = [ path.join( __dirname, "..", "phantomjs-glyphhanger.js" ), false, false, "", path.join( __dirname, "test-onload-content.html" ) ];
 
 		it( "should have 9 distinct glyphs", function( done ) {
 			this.timeout( 30000 );
 			childProcess.execFile( phantomjs.path, args, function( error, stdout, stderr ) {
 
-				assert.equal( "abcdefghi", stdout.trim() );
+				assert.equal( stdout.trim(), "abcdefghi" );
 				done();
 			});
 		})
@@ -110,7 +211,7 @@ describe( "glyphhanger-spider", function() {
 			gh.parse( div );
 			var urls = gh.getUrls();
 
-			assert.deepEqual( [ "firstlink.html" ], urls );
+			assert.deepEqual( urls, [ "firstlink.html" ] );
 		});
 	});
 
@@ -124,7 +225,7 @@ describe( "glyphhanger-spider", function() {
 			gh.parse( div );
 			var urls = gh.getUrls();
 
-			assert.deepEqual( [ "firstlink.html", "secondlink.html" ], urls );
+			assert.deepEqual( urls, [ "firstlink.html", "secondlink.html" ] );
 		});
 	});
 
@@ -138,7 +239,7 @@ describe( "glyphhanger-spider", function() {
 			gh.parse( div );
 			var urls = gh.getUrls();
 
-			assert.deepEqual( [ "firstlink.html" ], urls );
+			assert.deepEqual( urls, [ "firstlink.html" ] );
 		});
 	});
 
@@ -156,7 +257,7 @@ describe( "glyphhanger-spider", function() {
 					"file://" + path.join( __dirname, "urls.html" )
 				];
 
-				assert.equal( expecting.join( "\n" ), stdout.trim() );
+				assert.equal( stdout.trim(), expecting.join( "\n" ) );
 				done();
 			});
 		})
