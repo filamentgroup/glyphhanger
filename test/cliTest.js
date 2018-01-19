9e36e40cdf83f380742d85aa710640482d4df274
Zach Leatherman <zachleatherman@gmail.com>
2018-01-19 14:22:31 -0600

    debugging info

--- test/cliTest.js
+++ test/cliTest.js
@@ -2,14 +2,17 @@ var assert = require( "assert" );
 var path = require( "path" );
 var childProcess = require( "child_process" );
 var fs = require( "fs" );
+var path = require( "path" );
 
 var fontPath = "test/fonts/sourcesanspro-regular.ttf"
 
 describe( "glyphhanger cli", function() {
 	it( "Produced a file", function () {
 		this.timeout( 10000 );
-
-		let output = childProcess.execSync(`node index.js --whitelist=ABC --subset=${fontPath} --formats=ttf`);
+		console.log( "__dirname:", __dirname );
+		let output = childProcess.execSync(`node index.js --whitelist=ABC --subset=${fontPath} --formats=ttf`, {
+			cwd: path.resolve(__dirname, "..")
+		});
 		console.log( "childProcess output: ", output.toString() );
 
 		var subsetPath = fontPath.split( ".ttf" ).join( "-subset.ttf" );
